1129ba38dc0d731d4927d093a47b9651d1d82153
Victor Murilo <victor2706.vm@gmail.com>
2022-04-25 17:24:24 -0300

    Cumpri o requisito 2

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let inputTexto = document.querySelector('#text-input')
 inputTexto.addEventListener('keyup', function () {
     let memeTexto = document.querySelector('#meme-text')
+    memeTexto.id = 'meme-text'
     memeTexto.innerText = inputTexto.value 
 }) 
+
+
+let botaoMemeInsert = document.querySelector('#meme-insert')
+let containerMeme = document.querySelector('#meme-image-container')
+
+botaoMemeInsert.addEventListener('change', function (event) { // https://www.codegrepper.com/code-examples/javascript/how+to+show+uploaded+image+in+js <-- Me ajudou a fazer essa parte.
+    let image = document.querySelector('#meme-image')
+    image.src = URL.createObjectURL(event.target.files[0])
+    containerMeme.appendChild(image)
+})
