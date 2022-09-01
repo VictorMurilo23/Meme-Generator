b52fa423a42d0111f811840ea4c6a0bb3ef47e50
Victor-Murilo <victor2706.vm@gmail.com>
2022-09-01 12:26:05 -0300

    Arrumei a legenda, adicionei mais um botao de borda e deixei melhor pra acessar no celular

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const inputTexto = document.querySelector('#text-input');
-inputTexto.addEventListener('keyup', () => { // Texto do meme
-  const memeTexto = document.querySelector('#meme-text');
-  memeTexto.id = 'meme-text';
-  memeTexto.innerText = inputTexto.value;
-});
-
-const botaoMemeInsert = document.querySelector('#meme-insert');
+const memeInsertBtn = document.querySelector('#meme-insert');
 const containerMeme = document.querySelector('#meme-image-container');
 const image = document.querySelector('#meme-image');
+const inputText = document.querySelector('#text-input');
+const memeText = document.querySelector('#meme-text');
+const meme1 = document.querySelector('#meme-1');
+const meme2 = document.querySelector('#meme-2');
+const meme3 = document.querySelector('#meme-3');
+const meme4 = document.querySelector('#meme-4');
 
-botaoMemeInsert.addEventListener('change', (event) => { // https://www.codegrepper.com/code-examples/javascript/how+to+show+uploaded+image+in+js <-- Me ajudou a fazer essa parte.
+// let srcUploadImage = null;
+
+inputText.addEventListener('keyup', () => { // Texto do meme
+  memeText.id = 'meme-text';
+  memeText.innerText = inputText.value;
+});
+
+memeInsertBtn.addEventListener('change', (event) => { // https://www.codegrepper.com/code-examples/javascript/how+to+show+uploaded+image+in+js <-- Me ajudou a fazer essa parte.
 // Imagem meme
   image.src = URL.createObjectURL(event.target.files[0]);
 });
@@ -25,23 +31,23 @@ document.querySelector('#water').addEventListener('click', () => { // Botão wat
 document.querySelector('#earth').addEventListener('click', () => { // Botão earth
   containerMeme.style.border = '6px groove green';
 });
-// ------------------------------
-const meme1 = document.querySelector('#meme-1');
+
+document.querySelector('#black').addEventListener('click', () => {
+  containerMeme.style.border = '1px solid black';
+});
+
 meme1.addEventListener('click', () => {
   image.src = meme1.src;
 });
 
-const meme2 = document.querySelector('#meme-2');
 meme2.addEventListener('click', () => {
   image.src = meme2.src;
 });
 
-const meme3 = document.querySelector('#meme-3');
 meme3.addEventListener('click', () => {
   image.src = meme3.src;
 });
 
-const meme4 = document.querySelector('#meme-4');
 meme4.addEventListener('click', () => {
   image.src = meme4.src;
 });
